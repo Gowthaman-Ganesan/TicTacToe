218db1dfb1044b1ecb1b3e3fd675021fb9142072
Gowthaman Ganesan <gowthaman.ganesan@mallow-tech.com>
2020-06-24 01:35:32 +0530

    Added css

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {IoIosRadioButtonOff, IoMdClose} from "react-icons/io";
 
 export default function Cell(props) {
     return (
-        <button className={'cell'} onClick={props.onButtonClick}>
-            {props.value}
+        <button className={`cell ${props.value ? 'fade' : ''}`} onClick={props.onButtonClick}>
+            {props.value === 'X' ? <IoMdClose color={'red'}/> : props.value ? <IoIosRadioButtonOff color={"blue"}/>: ''}
         </button>
     )
 }
