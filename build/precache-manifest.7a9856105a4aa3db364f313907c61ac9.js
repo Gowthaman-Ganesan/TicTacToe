ef9dffbfe56746d3f4b9a740838e277b4956c622
Gowthaman Ganesan <gowthaman.ganesan@mallow-tech.com>
2020-06-24 01:55:02 +0530

    Deployment with 3

---
+++ build/precache-manifest.7a9856105a4aa3db364f313907c61ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf8f3eed633efd18b9ba47fa8aa9413",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "8b004ba3db4f9dbcf547",
+    "url": "/TicTacToe/static/css/2.1538a9e3.chunk.css"
+  },
+  {
+    "revision": "ad1e432c098e036f423b",
+    "url": "/TicTacToe/static/css/main.0b3491eb.chunk.css"
+  },
+  {
+    "revision": "8b004ba3db4f9dbcf547",
+    "url": "/TicTacToe/static/js/2.86f62985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.86f62985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1e432c098e036f423b",
+    "url": "/TicTacToe/static/js/main.a6b127bc.chunk.js"
+  },
+  {
+    "revision": "114df268d5636e9df8bb",
+    "url": "/TicTacToe/static/js/runtime-main.1d1014e6.js"
+  }
+]);
