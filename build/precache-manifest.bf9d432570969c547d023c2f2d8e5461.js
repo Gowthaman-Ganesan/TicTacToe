8a34d61f94eafc4db9a278ab07cc8d8d0d56f626
Gowthaman Ganesan <gowthaman.ganesan@mallow-tech.com>
2020-06-23 19:46:16 +0530

    deployment files

---
+++ build/precache-manifest.bf9d432570969c547d023c2f2d8e5461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44350f05a51424d040c8c361dfdcaa72",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "1e12b1bbdbd5fd714c7e",
+    "url": "/TicTacToe/static/css/main.c68ba0fb.chunk.css"
+  },
+  {
+    "revision": "8b1fe3bd90d2dd638d40",
+    "url": "/TicTacToe/static/js/2.cb4c2d8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.cb4c2d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e12b1bbdbd5fd714c7e",
+    "url": "/TicTacToe/static/js/main.928e6e55.chunk.js"
+  },
+  {
+    "revision": "114df268d5636e9df8bb",
+    "url": "/TicTacToe/static/js/runtime-main.1d1014e6.js"
+  }
+]);
